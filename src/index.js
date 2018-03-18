8904a440ec18c98378b894e84a62ed6096725740
james sewell <jamesjosephsewell@gmail.com>
2018-03-18 18:30:02 -0500

    got hot module replacement working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 console.log('test')
 console.log('this is line 2')
+console.log('this is on line 3')
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
 const webpack = require('webpack'); //to access built-in plugins
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -15,10 +16,22 @@ const config = {
   },
   devtool: 'inline-source-map',
   devServer: {
-        contentBase: './dist'
+      contentBase: './dist',
+      port: 3000,
+      hot: true,
+      open: true,
+      watchOptions: {
+        poll: true
+      }
     },
   plugins: [
-    new HtmlWebpackPlugin({template: './index.html'})
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({template: './index.html'}),
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ]
 };
 
