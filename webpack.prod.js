9c1b02beab1683ca6697b9e9efbe50c27fe4edef
james sewell <jamesjosephsewell@gmail.com>
2018-03-19 22:20:13 -0500

    got postcss working for build

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,17 +4,21 @@ const common = require('./webpack.common.js');
 const webpack = require('webpack'); //to access built-in plugins
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
 module.exports = merge(common, {
+    
     target: 'web',
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-              fallback: 'style-loader',
-              use: ['css-loader', 'sass-loader']
-            })
-          }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                  fallback: 'style-loader',
+                  use: [
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader', 'sass-loader'
+                  ]
+                })
+            }]
     },
     plugins: [
         new UglifyJSPlugin(),
